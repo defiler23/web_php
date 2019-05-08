d59fdafad933209ed4a57861737b3731481b2c4d
defiler23 <50418068+defiler23@users.noreply.github.com>
2019-05-08 18:50:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,56 +2,54 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Ejercicio - 7E2</title>
 </head>
 <body>
-<?php 
-    session_start();
-    $muestra = false;
-    //session_destroy();
-
-?>
-    
-<?php 
-if(!isset($_SESSION["suma"]) && !isset($_SESSION["lista"])){
-        $_SESSION["suma"]=0;
-        $_SESION["lista"]=" ";
-    } else {
-        $muestra = true;
-    }
-    
-if(isset($_REQUEST["total"]) && isset($_REQUEST["producto"]) ){
-        $muestra = true;
-        $_SESSION["suma"]=$_SESSION["suma"]+$_REQUEST["total"];
-        if(!isset($_SESSION["lista"])){
-            $_SESSION["lista"]=$_REQUEST["producto"]." ".$_REQUEST["total"]."<br>";
-        } else {
-            $_SESSION["lista"]=$_SESSION["lista"].$_REQUEST["producto"]." ".$_REQUEST["total"]."<br>";
-    }
-}
-    
-if($muestra){
-        echo "<h3>Total a gastar: ".$_SESSION["suma"]."<br><h3/>";
-        echo $_SESSION["lista"];
-    }
+   <?php 
+    $alumno = array("Joan", "Hector", "Gerard", "Joan", "Hector", "Gerard");
+    $asignatura = array("Matematicas", "Biologia", "Física", "Fisica", "Religion", "Biologia");
+    $nota = array("8", "9", "10", "10", "10", "8");
+        
+    function notasAlumno($nombre){
+            global $alumno, $asignatura, $nota;
     ?>
-
-<p><b>Productos disponibles:</b></p>
-<ul>
-<li>
-<a href="<?php echo $_SERVER['PHP_SELF'] . '?total=80' . '&producto=<b>Pantalones decarton<b/>'  ?>">Pantalones decarton 50</a></li>
-<li> 
-<a href="<?php echo $_SERVER['PHP_SELF'] . '?total=20' . '&producto=<b>Zapatillas Lacoste<b/>'  ?>">Zapatillas Lacoste 30</a></li>
-<li>  
-<a href="<?php echo $_SERVER['PHP_SELF'] . '?total=60' . '&producto=<b>Camiseta Louis Piton<b/>'  ?>">Camiseta Louis Piton 25</a></li>
-<li>  
-<a href="<?php echo $_SERVER['PHP_SELF'] . '?total=80' . '&producto=<b>Polo Ralf Laua<b/>'  ?>">Polo Ralf Laua 55</a> <br><br></li>
-</ul>
-
-
-    
-  
+    <table border="1">
+        <tr>
+            <td colspan="2"><?=$nombre; ?></td>
+        </tr>
+        <?php
+            for ($i = 0; $i < count($alumno); $i++){ 
+                if($nombre == $alumno[$i]){ 
+        ?>
+                <tr>
+                    <td><?=$asignatura[$i]; ?></td>
+                    <td><?=$nota[$i]; ?></td>
+                </tr>
+                <br/>
+        <?php
+                } 
+        }
+        $suma = 0;
+        $num = 0;
+        for ($i=0; $i<count($nota);$i++){
+            if ($nombre == $alumno[$i]){
+                $suma += $nota[$i];
+                $num++;
+            }
+        }
+        $media = $suma/$num;
+        ?>
+                <tr>
+                    <td>Media: </td>
+                    <td><?=$media;?></td>
+                </tr>
+    <?php
+    }
+    notasAlumno("Joan");
+    notasAlumno("Hector");
+    notasAlumno("Gerard");
+    ?>
+       
+    </table>
 </body>
 </html>
