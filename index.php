3a2c2c8c6665b3fbe029e6368125534bc61af21d
defiler23 <50418068+defiler23@users.noreply.github.com>
2019-05-08 18:57:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,54 +2,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Ejercicio - 7E2</title>
+    <title>Ej. Array-Tabla</title>
 </head>
 <body>
-   <?php 
-    $alumno = array("Joan", "Hector", "Gerard", "Joan", "Hector", "Gerard");
-    $asignatura = array("Matematicas", "Biologia", "Física", "Fisica", "Religion", "Biologia");
-    $nota = array("8", "9", "10", "10", "10", "8");
+    <?php 
+        $alumno = array("Joan", "Hector", "Gerard");
+        $asignatura = array("Matematicas", "Biologia", "Física");
+        $nota = array("8", "9", "10");
         
-    function notasAlumno($nombre){
-            global $alumno, $asignatura, $nota;
     ?>
     <table border="1">
         <tr>
-            <td colspan="2"><?=$nombre; ?></td>
+            <td>Alumno</td>
+            <td>Asignatura</td>
+            <td>Nota</td>
         </tr>
-        <?php
-            for ($i = 0; $i < count($alumno); $i++){ 
-                if($nombre == $alumno[$i]){ 
+        <?php 
+            for ($i = 0; $i < count($alumno); $i++){
         ?>
-                <tr>
-                    <td><?=$asignatura[$i]; ?></td>
-                    <td><?=$nota[$i]; ?></td>
-                </tr>
-                <br/>
-        <?php
-                } 
-        }
-        $suma = 0;
-        $num = 0;
-        for ($i=0; $i<count($nota);$i++){
-            if ($nombre == $alumno[$i]){
-                $suma += $nota[$i];
-                $num++;
-            }
-        }
-        $media = $suma/$num;
-        ?>
-                <tr>
-                    <td>Media: </td>
-                    <td><?=$media;?></td>
-                </tr>
-    <?php
-    }
-    notasAlumno("Joan");
-    notasAlumno("Hector");
-    notasAlumno("Gerard");
-    ?>
-       
+           <tr>
+            <td>
+                <?= $alumno[$i]; ?>
+            </td>
+            <td>
+                <?= $asignatura[$i]; ?>
+            </td>
+            <td>
+                <?= $nota[$i]; ?>
+            </td>
+        </tr>
+        <?php } ?>        
     </table>
 </body>
 </html>
